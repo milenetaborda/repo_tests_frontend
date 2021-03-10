d88f13fe2ceb25569dfd6f4c05802762f70e9c46
Milene Taborda <milenetaborda1@outlook.com>
2021-03-10 17:12:47 -0300

    style: refactored span tag style

--- belezanaweb-test-frontend/src/pages/Cart/styles.js
+++ belezanaweb-test-frontend/src/pages/Cart/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const ContainerCart = styled.div`
+export const CartContainer = styled.div`
   display: flex;
   flex-direction: column;
 
@@ -11,7 +11,7 @@ export const ContainerCart = styled.div`
     line-height: 17px;
     text-transform: uppercase;
 
-    margin: 10px 20px;
+    margin: 20px 0 5px 20px;
   }
 
   section {
@@ -21,23 +21,35 @@ export const ContainerCart = styled.div`
 
     > div {
       display: flex;
+      justify-content: space-between;
       list-style-type: none;
       border: 1px solid #eee;
       border-radius: 3px;
-      padding: 0.75rem;
       margin: 0.75rem;
 
       img {
         height: 64px;
         width: 64px;
-        margin: 10px;
+        margin: 15px;
       }
 
       span {
-        margin: 2vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
         color: #212122;
         font-size: 13px;
         line-height: 16px;
+        text-align: right;
+        margin: 15px;
+
+        strong {
+          color: #212122;
+          font-size: 14px;
+          font-weight: 700;
+          line-height: 17px;
+          margin-top: 15px;
+        }
       }
     }
   }
