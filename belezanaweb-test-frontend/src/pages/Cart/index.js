1a793ff913a6daf2711d4b65fd1b3ff4b6f99271
Milene Taborda <milenetaborda1@outlook.com>
2021-03-10 13:08:35 -0300

    feat: rendering of cart products

--- belezanaweb-test-frontend/src/pages/Cart/index.js
+++ belezanaweb-test-frontend/src/pages/Cart/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../sevices/api'
+import { formatPrice } from '../../utils/format'
 
 import { ContainerCart } from './styles'
 
@@ -7,12 +8,13 @@ function Cart() {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    api.get('/').then((response) => {
+    api.get('/products').then((response) => {
       setProducts(response.data)
     })
+  }, [])
 
-    console.log(products)
-  }, [products])
+  const response = products.items?.map((item) => item.product)
+  console.log(products)
 
   return (
     <ContainerCart>
@@ -20,26 +22,15 @@ function Cart() {
       <h1>Produtos</h1>
 
       <section>
-        <div>
-          <img
-            src="https://www.maybelline.com.br/~/media/mny/global/new%20hp%20assets/hp%20video%20carousel/iar%20concealer/iar_pack_us.png"
-            alt="Imagem do produto"
-          />
-          <span>
-            <p>L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium</p>
-            <strong>200,00</strong>
-          </span>
-        </div>
-        <div>
-          <img
-            src="https://www.maybelline.com.br/~/media/mny/global/new%20hp%20assets/hp%20video%20carousel/iar%20concealer/iar_pack_us.png"
-            alt="Imagem do produto"
-          />
-          <span>
-            <p>L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium</p>
-            <strong>200,00</strong>
-          </span>
-        </div>
+        {response?.map((product) => (
+          <div key={product.name}>
+            <img src={product.imageObjects[0].small} alt="Imagem do produto" />
+            <span>
+              <p>{product.name}</p>
+              <strong>{formatPrice(product.priceSpecification.price)}</strong>
+            </span>
+          </div>
+        ))}
       </section>
     </ContainerCart>
   )
