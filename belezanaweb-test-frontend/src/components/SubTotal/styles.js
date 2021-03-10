7efbb21a4ef2d67202f4321177ff0c5487099f5c
Milene Taborda <milenetaborda1@outlook.com>
2021-03-10 17:09:19 -0300

    style: creating of the SubTotal component style

---
+++ belezanaweb-test-frontend/src/components/SubTotal/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const SubTotalContainer = styled.div`
+  border-radius: 3px;
+  border: 1px solid #ccc;
+  margin: 20px 0;
+  text-transform: uppercase;
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+    margin: 0.75rem;
+  }
+
+  div:nth-of-type(3) {
+    color: #ff7800;
+  }
+  div:nth-of-type(4) {
+    padding-top: 8px;
+    font-weight: bold;
+  }
+`
