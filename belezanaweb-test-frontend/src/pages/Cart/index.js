6c6d3c53cef14d2a220482db41163112e374cef8
Milene Taborda <milenetaborda1@outlook.com>
2021-03-12 14:04:00 -0300

    chore: removal of unused imports

--- belezanaweb-test-frontend/src/pages/Cart/index.js
+++ belezanaweb-test-frontend/src/pages/Cart/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { SubTotalComponent } from '../../components/SubTotal'
